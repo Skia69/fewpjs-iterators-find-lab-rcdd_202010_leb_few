2f376b3f65704fb12269ed3855f6c06534c4c8fd
Mhamad Ibrahim <skiastep@gmail.com>
2020-12-10 12:11:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = (arr) => { 
+  let result = arr.find(el => el.year === 'W')
+  if(!!result) {
+    return result.year
+  }else {
+  return undefined }
+}
